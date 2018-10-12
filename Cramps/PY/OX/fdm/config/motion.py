9d5712c0802b577e206b46158a52871861693668
Michael Brown <producer@holotronic.dk>
2018-10-12 12:48:05 +0000

    Change to DE0 Nano Soc board
    Signed-off-by: Michael Brown <producer@holotronic.dk>

--- Cramps/PY/OX/fdm/config/motion.py
+++ Cramps/PY/OX/fdm/config/motion.py
@@ -12,7 +12,7 @@ def setup_motion(kinematics='trivkins'):
 #    rt.newinst(c.find('HOSTMOT2', 'DRIVER'),
 #        c.find('HOSTMOT2', 'DEVNAME'),
 #        c.find('HOSTMOT2', 'CONFIG'))
-    os.system('halcmd newinst hm2_soc_ol hm2-socfpga0 -- config="firmware=socfpga/dtbo/DE10_Nano_FB_Cramps.3x24_cap.dtbo num_pwmgens=1 num_stepgens=4" debug=1')
+    os.system('halcmd newinst hm2_soc_ol hm2-socfpga0 -- config="firmware=socfpga/dtbo/DE0_Nano_SoC_Cramps.3x24_cap.dtbo num_pwmgens=1 num_stepgens=4" debug=1')
 
     # motion controller, get name and thread periods from ini file
     rt.loadrt(c.find('EMCMOT', 'EMCMOT'),
